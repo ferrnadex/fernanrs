ff1c991cc7faa63802858ab780a3bef21942e4d1
Fernan <fernanr417@gmail.com>
2024-07-23 20:09:24 -0400

    Adding text on homepage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! My name is Fernan I am currently working on this website. Sorry, I am not using AI. I will take a moment!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/ferrnadex/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn @in/ferrnadex
         </a>
       </header>
     </div>
